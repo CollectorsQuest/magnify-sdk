5f9491c11bc011c0cc1b55475c84c12c56531778
Yanko Simeonoff <yanko@zecho.info>
2012-03-29 18:14:50 +0300

    Added base tests

---
+++ tests/unit/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * File: bootstrap.php
+ *
+ * @author zecho
+ * @version $Id$
+ *
+ */
+
+defined('MAGNIFY_CHANNEL') || define('MAGNIFY_CHANNEL', 'collectors-quest.magnify.net');
+defined('MAGNIFY_API_KEY') || define('MAGNIFY_API_KEY', 'Q4X6W30XSVC4SKGH');
