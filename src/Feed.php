aa254d1758b58307c77c544a2433d852df9bc9de
Yanko Simeonoff <yanko@zecho.info>
2012-05-08 10:12:31 +0300

    Implemented Countable, ArrayAccess to MagnifyFeed

--- src/Feed.php
+++ src/Feed.php
@@ -27,7 +27,7 @@
  * Id: $Id$
  */
 
-class MagnifyFeed implements Iterator
+class MagnifyFeed implements Iterator, Countable, ArrayAccess
 {
 
   public $title, $content, $updated;
@@ -209,4 +209,79 @@ class MagnifyFeed implements Iterator
     return $this->user;
   }
 
+  /**
+   * (PHP 5 &gt;= 5.1.0)<br/>
+   * Count elements of an object
+   * @link http://php.net/manual/en/countable.count.php
+   * @return int The custom count as an integer.
+   * </p>
+   * <p>
+   * The return value is cast to an integer.
+   */
+  public function count()
+  {
+    return $this->totalResults;
+  }
+
+  /**
+   * (PHP 5 &gt;= 5.1.0)<br/>
+   * Whether a offset exists
+   * @link http://php.net/manual/en/arrayaccess.offsetexists.php
+   * @param mixed $offset <p>
+   * An offset to check for.
+   * </p>
+   * @return boolean Returns true on success or false on failure.
+   * </p>
+   * <p>
+   * The return value will be casted to boolean if non-boolean was returned.
+   */
+  public function offsetExists($offset)
+  {
+    return isset($this->_entries[$offset]);
+  }
+
+  /**
+   * (PHP 5 &gt;= 5.1.0)<br/>
+   * Offset to retrieve
+   * @link http://php.net/manual/en/arrayaccess.offsetget.php
+   * @param mixed $offset <p>
+   * The offset to retrieve.
+   * </p>
+   * @return mixed Can return all value types.
+   */
+  public function offsetGet($offset)
+  {
+    return $this->_entries[$offset];
+  }
+
+  /**
+   * (PHP 5 &gt;= 5.1.0)<br/>
+   * Offset to set
+   * @link http://php.net/manual/en/arrayaccess.offsetset.php
+   * @param mixed $offset <p>
+   * The offset to assign the value to.
+   * </p>
+   * @param mixed $value <p>
+   * The value to set.
+   * </p>
+   * @return void
+   */
+  public function offsetSet($offset, $value)
+  {
+    $this->_entries[$offset] = $value;
+  }
+
+  /**
+   * (PHP 5 &gt;= 5.1.0)<br/>
+   * Offset to unset
+   * @link http://php.net/manual/en/arrayaccess.offsetunset.php
+   * @param mixed $offset <p>
+   * The offset to unset.
+   * </p>
+   * @return void
+   */
+  public function offsetUnset($offset)
+  {
+    unset($this->_entries[$offset]);
+  }
 }
